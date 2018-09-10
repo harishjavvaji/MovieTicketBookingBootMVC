a105e5977b93a1c293f4cb12978fc8e3ed8b079e
harishjavvaji <42327832+harishjavvaji@users.noreply.github.com>
2018-09-10 16:08:55 -0400

    Movie Controller is updated with connection to REST

--- src/main/java/com/movieticket/booking/controllers/MovieController.java
+++ src/main/java/com/movieticket/booking/controllers/MovieController.java
@@ -1,11 +1,13 @@
 package com.movieticket.booking.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+import com.movieticket.booking.models.*;
 
 @Controller
 public class MovieController {
@@ -16,9 +18,24 @@ public class MovieController {
     public ModelAndView viewMovies() {
         ModelAndView modelAndView = new ModelAndView("movies");
 
+        ResponseEntity<Movie[]> responseEntity =
+                restTemplate.getForEntity("/url", Movie[].class);
+        int statusCode = responseEntity.getStatusCodeValue();
+
+        if (statusCode >= 200 && statusCode <= 299) {
+            Movie[] movies = responseEntity.getBody();
+            modelAndView.addObject("moviesArray", movies);
+        }else {
+            modelAndView.addObject("Server is temporarily down");
+        }
+
         return modelAndView;
 
     }
+
+    
+
+
 
 
 
