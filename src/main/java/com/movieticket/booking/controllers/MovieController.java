7d546b7d14b2b7db0cd56d3196b5c861eb2aa649
harishjavvaji <42327832+harishjavvaji@users.noreply.github.com>
2018-09-10 15:30:13 -0400

    Controllers added

--- src/main/java/com/movieticket/booking/controllers/MovieController.java
+++ src/main/java/com/movieticket/booking/controllers/MovieController.java
@@ -1,19 +1,25 @@
 package com.movieticket.booking.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MovieController {
 
+    @Autowired
+    RestTemplate restTemplate;
     @RequestMapping(value = "/movies", method = RequestMethod.GET)
     public ModelAndView viewMovies() {
         ModelAndView modelAndView = new ModelAndView("movies");
+
         return modelAndView;
 
     }
+
 
 
 
