f7fe4d7ef508581480ed5277489577a8c41b4eaa
SukanyaVanka <sukanyavanka@gmail.com>
2018-09-10 15:02:05 -0400

    Customer pogo class created

---
+++ src/main/java/com/movieticket/booking/models/Payment.java
@@ -0,0 +1,52 @@
+package com.movieticket.booking.models;
+
+public class Payment {
+
+    private String cardNumber;
+    private String cardHolderName;
+    private int month;
+    private int year;
+
+    public Payment() {
+    }
+
+    public Payment(String cardNumber, String cardHolderName, int month, int year) {
+
+        this.cardNumber = cardNumber;
+        this.cardHolderName = cardHolderName;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
