053a043214ecd31e73d3513317658fd82b0beae9
harishjavvaji <42327832+harishjavvaji@users.noreply.github.com>
2018-09-10 20:58:11 -0400

    All controllers are updated with RESTful services

--- src/main/java/com/movieticket/booking/controllers/LoginController.java
+++ src/main/java/com/movieticket/booking/controllers/LoginController.java
@@ -1,18 +1,54 @@
 package com.movieticket.booking.controllers;
 
+import com.movieticket.booking.models.Customer;
+import com.movieticket.booking.services.LoginService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.security.auth.login.LoginContext;
+import javax.validation.Valid;
+
 @Controller
+@SessionAttributes("customer")
 public class LoginController {
+    @Autowired
+    LoginService loginService;
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView viewLogin() {
 
         ModelAndView modelAndView = new ModelAndView("login");
+        modelAndView.addObject(new Customer());
         return modelAndView;
 
+    }
 
+    @RequestMapping(value = "/validateLogin", method = RequestMethod.POST)
+    public ModelAndView validateLogin(@Valid @ModelAttribute("customer")Customer customer, Errors errors) {
+        if (errors.hasErrors()) {
+
+            ModelAndView modelAndView = new ModelAndView("login");
+            modelAndView.addObject("error", errors.getAllErrors());
+            return modelAndView;
+        }
+
+        Boolean validation = loginService.validateLogin(customer);
+
+        if (validation.equals(true)) {
+            return new ModelAndView("loginhome");
+
+        }else {
+            ModelAndView modelAndView = new ModelAndView("login");
+            modelAndView.addObject("validationError", "Invalid Username or Password");
+            return modelAndView;
+        }
     }
 }
--- src/main/java/com/movieticket/booking/controllers/MovieController.java
+++ src/main/java/com/movieticket/booking/controllers/MovieController.java
@@ -1,13 +1,17 @@
 package com.movieticket.booking.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
 import com.movieticket.booking.models.*;
+
+import javax.validation.Valid;
 
 @Controller
 public class MovieController {
@@ -33,7 +37,26 @@ public class MovieController {
 
     }
 
-    
+    @RequestMapping(value = "/bookmovie", method = RequestMethod.GET)
+    public ModelAndView bookMovie(@Valid @ModelAttribute("movie") Movie movie) {
+        ModelAndView modelAndView = new ModelAndView("theatres");
+
+        ResponseEntity<Theatre[]> responseEntity =
+        restTemplate.getForEntity("/url", Theatre[].class);
+
+        int statusCode = responseEntity.getStatusCodeValue();
+        if (statusCode >= 200 && statusCode <= 299) {
+            Theatre[] theatres = responseEntity.getBody();
+            modelAndView.addObject("theatres", theatres);
+        }else
+            modelAndView.addObject("Server is temporarily down");
+
+        return modelAndView;
+    }
+
+
+
+
 
 
 
