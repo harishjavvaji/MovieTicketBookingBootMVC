8b299af87538b0f997c939f6dbcf4f7f1019a5fc
harishjavvaji <42327832+harishjavvaji@users.noreply.github.com>
2018-12-05 21:25:00 -0600

    Created a pojo class called DEMO

---
+++ src/main/java/com/movieticket/booking/models/Demo.java
@@ -0,0 +1,29 @@
+package com.movieticket.booking.models;
+
+
+public class Demo {
+
+    private String abc;
+    private String def;
+
+    public Demo(String abc, String def) {
+        this.abc = abc;
+        this.def = def;
+    }
+
+    public String getAbc() {
+        return abc;
+    }
+
+    public void setAbc(String abc) {
+        this.abc = abc;
+    }
+
+    public String getDef() {
+        return def;
+    }
+
+    public void setDef(String def) {
+        this.def = def;
+    }
+}
